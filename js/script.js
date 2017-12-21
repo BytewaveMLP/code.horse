afefb9f1467201060a4d74965a1921252e98c8ca
mingmingrr <mingmingrr@gmail.com>
2017-12-20 20:05:53 -0500

    Complete rewrite of index page
    Notable changes:
    
    - Now using Pug + Stylus + Livescript
    - Font is now actually monospace (and more wacky)
    - Font and background color rules are applied globalls (with exceptions)
    - ::selection pseudo-element rules are applied globally (with exceptions)
    - Cursor animation is done with borders instead of blinking a character
    - Lists are now tables
    - Prompt strings and commands get their own separate spans
    - Scanlines are somewhat simplified
    - Typing animation is done by changing prompt width instead of adding chars
    - JS is much more general: going down the document and handling each
      prompt/response block with separate events based on timing config
    
    P.S. Don't remove trailing whitespace in script.ls unless you plan on
    joining the lines

--- js/script.js
+++ js/script.js
@@ -1,118 +1,73 @@
-// Welcome to callback hell!
-// Population: 0!
-
-let delayMul = 1;
-
-$(function() {
-	$('.container').hide();
-	$('.start-hidden').hide();
-	
-	$(document.body).css('background-color', 'black');
-
-	$(document.body).delay(1000).animate({
-		backgroundColor: '#282828'
-	}, 1000, 'swing', showDesc);
-
-	$('#skip-animation').click(function(e) {
-		e.preventDefault();
-
-		$('#skip-animation').fadeOut(250);
-
-		delayMul = 0;
-
-		return false;
-	});
+var timings, trace, skipPrompts, showNextBlock, this$ = this;
+document.querySelectorAll('.prompt,.response').forEach(function(it){
+  return it.classList.add('hidden');
 });
-
-function showDesc() {
-	$('.container').fadeIn(250 * delayMul, function() {
-		$('#skip-animation').fadeIn(250);
-
-		$('#whoami').typed({
-			strings: ['whoami'],
-			startDelay: 500 * delayMul,
-			typeSpeed: 50 * delayMul,
-			cursorChar: '&#x2588;',
-			callback: function() {
-				$('.typed-cursor').hide();
-				$('#desc').delay(500 * delayMul).fadeIn(250 * delayMul, showLs);
-			}
-		});
-	});
-}
-
-function showLs() {
-	$('#ls-prompt').delay(500 * delayMul).fadeIn(250 * delayMul, function() {
-		$('#ls-cmd').typed({
-			strings: ['ls'],
-			startDelay: 2000 * delayMul,
-			typeSpeed: 50 * delayMul,
-			cursorChar: '&#x2588;',
-			callback: function() {
-				$('.typed-cursor').delay(500 * delayMul).hide();
-				$('#ls').delay(500 * delayMul).fadeIn(250 * delayMul, showProfiles);
-			}
-		});
-	});
-}
-
-function showProfiles() {
-	$('#profiles-prompt').delay(500 * delayMul).fadeIn(250 * delayMul, function() {
-		$('#cat-profiles').typed({
-			strings: ['cat profiles.txt'],
-			startDelay: 2000 * delayMul,
-			typeSpeed: 50 * delayMul,
-			cursorChar: '&#x2588;',
-			callback: function() {
-				$('.typed-cursor').delay(500 * delayMul).hide();
-				$('#profiles').delay(500 * delayMul).fadeIn(250 * delayMul, showProjects);
-			}
-		});
-	});
-}
-
-function showProjects() {
-	$('#projects-prompt').delay(500 * delayMul).fadeIn(250 * delayMul, function() {
-		$('#cat-projects').typed({
-			strings: ['cat projects.txt'],
-			startDelay: 2000 * delayMul,
-			typeSpeed: 50 * delayMul,
-			cursorChar: '&#x2588;',
-			callback: function() {
-				$('.typed-cursor').delay(500 * delayMul).hide();
-				$('#projects').delay(500 * delayMul).fadeIn(250 * delayMul, showLicense);
-			}
-		});
-	});
-}
-
-function showLicense() {
-	$('#license-prompt').delay(500 * delayMul).fadeIn(250 * delayMul, function() {
-		$('#cat-license').typed({
-			strings: ['cat license.txt'],
-			startDelay: 2000 * delayMul,
-			typeSpeed: 50 * delayMul,
-			cursorChar: '&#x2588;',
-			callback: function() {
-				$('.typed-cursor').delay(500 * delayMul).hide();
-				$('#license').fadeIn(250 * delayMul, showExit);
-			}
-		});
-	});
-}
-
-function showExit() {
-	$('#skip-animation').fadeOut(250);
-	$('#exit-prompt').delay(500 * delayMul).fadeIn(250 * delayMul, function() {
-		$('#exit-cmd').typed({
-			strings: ['exit'],
-			startDelay: 2000 * delayMul,
-			typeSpeed: 50 * delayMul,
-			cursorChar: '&#x2588;',
-			callback: function() {
-				$('.typed-cursor').delay(500 * delayMul).hide();
-				$('#exit').fadeIn(250 * delayMul);
-			}
-		});
-	});
-}
+timings = {
+  initialDelay: 1,
+  ps1Delay: 0.2,
+  ps1Show: 0.2,
+  commandDelay: 0.6,
+  commandShow: 0.15,
+  responseDelay: 0.5,
+  responseShow: 1
+};
+trace = function(){
+  console.log.apply(console, arguments);
+  return arguments[arguments.length - 1];
+};
+document.querySelector('#skip-button').addEventListener('click', skipPrompts = function(){
+  document.querySelectorAll('.prompt,.response').forEach(function(block){
+    var x$;
+    x$ = block;
+    x$.classList.remove('hidden');
+    x$.classList.remove('cursor');
+    x$.style.transition = '';
+  });
+  document.querySelector('#skip-button').classList.add('hidden');
+});
+showNextBlock = function(){
+  var block, ref$, ps1, command, x$, ps1Width, charCount, commandWidth, delay, y$;
+  if ((block = document.querySelector('.prompt.hidden,.response.hidden')) == null) {
+    return skipPrompts();
+  }
+  switch (false) {
+  case !block.classList.contains('prompt'):
+    ref$ = [block.querySelector('.ps1'), block.querySelector('.command')], ps1 = ref$[0], command = ref$[1];
+    block.classList.remove('hidden');
+    x$ = ps1.style;
+    x$.opacity = 0;
+    x$.transition = "opacity " + timings.ps1Show + "s";
+    ps1Width = ps1.offsetWidth;
+    block.style.width = ps1Width + "px";
+    charCount = command.textContent.length, commandWidth = command.offsetWidth;
+    block.style.transition = "width " + charCount * timings.commandShow + "s steps(" + charCount + ")";
+    setTimeout(function(){
+      ps1.style.opacity = 1;
+    }, 1000 * (delay = timings.ps1Delay));
+    setTimeout(function(){
+      block.classList.add('cursor');
+    }, 1000 * (delay += timings.ps1Show));
+    setTimeout(function(){
+      block.style.width = (ps1Width + commandWidth) + "px";
+    }, 1000 * (delay += timings.commandDelay));
+    setTimeout(function(){
+      block.classList.remove('cursor');
+      block.style.width = '';
+      showNextBlock();
+    }, 1000 * (delay += charCount * timings.commandShow));
+    break;
+  case !block.classList.contains('response'):
+    y$ = block;
+    y$.style.opacity = 0;
+    y$.classList.remove('hidden');
+    y$.style.transition = "opacity " + timings.responseShow + "s";
+    setTimeout(function(){
+      block.style.opacity = 1;
+    }, 1000 * timings.responseDelay);
+    setTimeout(showNextBlock, 1000 * (timings.responseShow + timings.responseDelay));
+    break;
+  default:
+    throw new Error('invalid command-line block');
+  }
+};
+setTimeout(showNextBlock, 1000 * timings.initialDelay);
